ec2141e37f51bc72a4c6e3881227665c8b38da6c
MrMax01 <massimo.mauro00@gmail.com>
2023-11-03 14:28:51 +0100

    first commit

---
+++ src/main/java/massimomauro/S5L5DevelopmentofaSpringBootapplication/S5L5DevelopmentOfASpringBootApplication.java
@@ -0,0 +1,13 @@
+package massimomauro.S5L5DevelopmentofaSpringBootapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5L5DevelopmentOfASpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5L5DevelopmentOfASpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/massimomauro/S5L5DevelopmentofaSpringBootapplication/S5L5DevelopmentOfASpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package massimomauro.S5L5DevelopmentofaSpringBootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5L5DevelopmentOfASpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
